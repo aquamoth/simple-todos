7908e11b607824be108b09a03a4f6615774be8b7
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-17 15:52:05 +0700

    Split code into functions for better overview. This is straying from the tutorial, but adds no functionality!

--- simple-todos.js
+++ simple-todos.js
@@ -1,42 +1,53 @@
+/* global Mongo */
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   //This code only runs on the client
+
   Template.body.helpers({
-    tasks: function() {
-        //Show newest tasks at the top 
-        return Tasks.find({}, {sort: {createdAt: -1}}); 
-      }
+    tasks: body_helper_tasks 
   });
   
   Template.body.events({
-    "submit .new-task": function(event){
-      //Prevent default browser form submit
-      event.preventDefault();
-      
-      //Get valie from form element
-      var text = event.target.text.value;
-      
-      //Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() //current time
-      });
-      
-      //Clear form
-      event.target.text.value='';
-    }
+    "submit .new-task": onSubmitNewTask 
   });
-  
+
   Template.task.events({
-    "click .toggle-checked": function(){
-      //Set the checked property to the oppisite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: !this.checked} 
-      });
-    },
-    "click .delete": function(){
-      Tasks.remove(this._id);
-    }
+    "click .toggle-checked": onTaskToggleChecked,
+    "click .delete": onTaskDelete
   });
+      
+  function body_helper_tasks() {
+    //Show newest tasks at the top 
+    return Tasks.find({}, {sort: {createdAt: -1}}); 
+  }
+
+  function onTaskToggleChecked() {
+    //Set the checked property to the oppisite of its current value
+    Tasks.update(this._id, {
+      $set: {checked: !this.checked} 
+    });
+  }
+  
+  function onTaskDelete() {
+    Tasks.remove(this._id);
+  }
+
+  function onSubmitNewTask(event){
+    //Prevent default browser form submit
+    event.preventDefault();
+    
+    //Get valie from form element
+    var text = event.target.text.value;
+    
+    //Insert a task into the collection
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() //current time
+    });
+    
+    //Clear form
+    event.target.text.value='';
+  }
+
 }
