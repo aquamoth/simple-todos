8f7793d75b73b23150f2ba5209796d3ce28b037a
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-17 20:46:31 +0700

    Added checkbox to only display uncompleted todos. Inlined functions again since Firefox doesn't find the functions otherwise.

--- simple-todos.js
+++ simple-todos.js
@@ -1,53 +1,59 @@
-/* global Mongo */
+/* global Meteor, Mongo, Session, Template, Tasks */
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   //This code only runs on the client
 
   Template.body.helpers({
-    tasks: body_helper_tasks 
+    tasks: function body_helper_tasks() {
+      //Show newest tasks at the top
+      if(Session.get("hideCompleted")){
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      }else{
+        return Tasks.find({}, {sort: {createdAt: -1}}); 
+      } 
+    },
+    hideCompleted: function body_helper_hideCompleted(){
+      return Session.get("hideCompleted");
+    } 
   });
   
   Template.body.events({
-    "submit .new-task": onSubmitNewTask 
+    "submit .new-task": function onSubmitNewTask(event){
+      //Prevent default browser form submit
+      event.preventDefault();
+      
+      //Get valie from form element
+      var text = event.target.text.value;
+      
+      //Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() //current time
+      });
+      
+      //Clear form
+      event.target.text.value='';
+    },
+    "change .hide-completed input": function onChangeHideCompleted(event){
+      Session.set("hideCompleted", event.target.checked);  
+    }
   });
 
   Template.task.events({
-    "click .toggle-checked": onTaskToggleChecked,
-    "click .delete": onTaskDelete
+    "click .toggle-checked": function onTaskToggleChecked() {
+      //Set the checked property to the oppisite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: !this.checked} 
+      });
+    },
+    "click .delete": function onTaskDelete() {
+      Tasks.remove(this._id);
+    }
   });
       
-  function body_helper_tasks() {
-    //Show newest tasks at the top 
-    return Tasks.find({}, {sort: {createdAt: -1}}); 
-  }
-
-  function onTaskToggleChecked() {
-    //Set the checked property to the oppisite of its current value
-    Tasks.update(this._id, {
-      $set: {checked: !this.checked} 
-    });
-  }
   
-  function onTaskDelete() {
-    Tasks.remove(this._id);
-  }
-
-  function onSubmitNewTask(event){
-    //Prevent default browser form submit
-    event.preventDefault();
-    
-    //Get valie from form element
-    var text = event.target.text.value;
-    
-    //Insert a task into the collection
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() //current time
-    });
-    
-    //Clear form
-    event.target.text.value='';
-  }
+  
+  
 
 }
